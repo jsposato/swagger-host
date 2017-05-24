28e4f3ce86cbbb4fbfed5b306f11bc7e3be2c30d
John Sposato <jsposato@gmail.com>
2017-05-24 19:50:27 -0400

    formatting change

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<head><title>Mobiquity Swagger Docs</title></head>
+<head>
+    <title>Mobiquity Swagger Docs</title>
+</head>
 <body>
 <h1>Swagger Projects</h1>
 <ul>

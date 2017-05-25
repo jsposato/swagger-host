9262896c2db7de65253f2382406f85286bf4de82
John Sposato <jsposato@gmail.com>
2017-05-24 20:06:30 -0400

    Add bootstrap for later prettiness

--- index.php
+++ index.php
@@ -1,27 +1,45 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <title>Mobiquity Swagger Docs</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+          crossorigin="anonymous">
+
 </head>
 <body>
-<h1>Swagger Projects</h1>
-<ul>
-    <?php
-    ini_set("display_errors", "1");
-    $directories = glob(__DIR__ . '/*', GLOB_ONLYDIR);
+<div class="container">
+    <div class="row">
+        <div class="col-xs-4">
 
-    foreach ($directories as $directory) {
-        // ignore the current directory
-        if ($directory == '/usr/share/nginx/html/*') {
-            continue;
-        }
-        ?>
-        <li><a href="<?php echo $directory ?>>"><?php echo ucwords(basename($directory)) ?></a></li>
+            <h1>Swagger Projects</h1>
+            <ul>
+                <?php
+                ini_set("display_errors", "1");
+                $directories = glob(__DIR__ . '/*', GLOB_ONLYDIR);
 
-        <?php
+                foreach ($directories as $directory) {
+                    // ignore the current directory
+                    if ($directory == '/usr/share/nginx/html/*') {
+                        continue;
+                    }
+                    ?>
+                    <li><a href="<?php echo $directory ?>>"><?php echo ucwords(basename($directory)) ?></a></li>
 
-    }
-    ?>
-</ul>
+                    <?php
 
+                }
+                ?>
+            </ul>
+        </div>
+
+    </div>
+</div>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+        crossorigin="anonymous"></script>
 </body>
 </html>
